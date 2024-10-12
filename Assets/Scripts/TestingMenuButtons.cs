7ceab91bea5186b336acb7d40a6e63a63095ece5
Emmanuelle Cloutier <darkforest1045@gmail.com>
2024-10-12 19:59:09 -0400

    Ajout audio + soundmanager

---
+++ Assets/Scripts/TestingMenuButtons.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingMenuButtons : MonoBehaviour
+{
+    public AudioClip nextSong;
+    public AudioClip btnSound;
+    
+    public void Play()
+    {
+        SoundPlayer.instance.SetMusic(nextSong,1F);
+        SoundPlayer.instance.PlaySFX(btnSound,2);
+    }
+}

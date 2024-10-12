ba045e19c09ee665bbbe94ad546c1e75626e0108
Coralie <coralie.tremblay12@icloud.com>
2024-10-11 20:33:29 -0400

    wip tower

--- Assets/Scripts/DonutTower.cs
+++ Assets/Scripts/DonutTower.cs
@@ -2,24 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface ITower
+public class DonutTower : Tower
 {
-    // Références
-    public Monster targetMonster;
-    public Projectile projectile;
-    public GameManager gameManager;
-
-    // Variables
-    public string towerType;
-    public int rayonTowerRayon;
-    public int donutTowerPetitRayon;
-    public int donutTowerGrandRayon;
-    public float distance;
-
     // Start is called before the first frame update
     void Start()
     {
-        targetMonster = null;
+        
     }
 
     // Update is called once per frame
@@ -28,22 +16,17 @@ public interface ITower
         
     }
 
-    
-
-    
-    /*
-    void RayonAttack()
+    void Attack()
     {
         foreach (gameManager.getMonsters() monster in Monster)
         {
             distance = Vector2.Distance(this.position, monster.position);
 
-            if (distance ­< rayonTowerRayon) {
+            if (distance ­< donutTowerGrandRayon && distance > donutTowerPetitRayon) {
                 targetMonster = monster;
 
                 break;
             }
         }
     }
-    *
 }

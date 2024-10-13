07c5bac996d4b608ea2d6ec2d556423a3afe8268
Emmanuelle Cloutier <darkforest1045@gmail.com>
2024-10-13 11:00:06 -0400

    levels options scenes

--- Assets/Scripts/TestingMenuButtons.cs
+++ Assets/Scripts/TestingMenuButtons.cs
@@ -6,10 +6,16 @@ public class TestingMenuButtons : MonoBehaviour
     public AudioClip nextSong;
     public AudioClip btnSound;
     
+    public void LevelOne()
+    {
+        //SceneManager.LoadScene("");
+        SoundPlayer.instance.SetMusic(nextSong,1F);
+        SoundPlayer.instance.PlaySFX(btnSound,2);
+    }
+    
     public void Play()
     {
-        SceneManager.LoadScene("SampleScene");
-        SoundPlayer.instance.SetMusic(nextSong,1F);
+        SceneManager.LoadScene("levelOptionScene");
         SoundPlayer.instance.PlaySFX(btnSound,2);
     }
     

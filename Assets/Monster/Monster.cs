331b1711efc75dad46aee93187f13053c9536b33
Emmanuelle Cloutier <darkforest1045@gmail.com>
2024-10-12 23:35:21 -0400

    money money money

--- Assets/Monster/Monster.cs
+++ Assets/Monster/Monster.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 namespace Monster
 {
+   
     public class Monster : MonoBehaviour
     {
+        public int amountToAdd;
+        
         // Variables
         public int life;
         public int power;
@@ -23,6 +26,7 @@ namespace Monster
         // Start is called before the first frame update
         void Start()
         { 
+           
             MapManager.mapManager.setList();
         }
 
@@ -60,6 +64,8 @@ namespace Monster
             if (life <= 0)
             {
                 Destroy(gameObject);
+                GameManager.gameManager.AddMoney(amountToAdd);
+                
             }
         }
 
